240d6ff530816fa616b8d95bfeeea6b1ce06a07b
oubijjane <79411385+oubijjane@users.noreply.github.com>
2026-01-27 12:11:25 +0100

    feat: Add WebSecurityCustomizer to ignore specific static resource paths

--- orderTracking/src/main/java/com/verAuto/orderTracking/security/SecurityConfig.java
+++ orderTracking/src/main/java/com/verAuto/orderTracking/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +44,11 @@ public class SecurityConfig {
     }
 
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/assets/**", "/static/**", "/verauto-logo.png", "/*.js", "/*.css");
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
